e97770cee79b4d7de0fc3baaf2b88c7e420d2cff
Adrian Dmitra <adrian@dmitra.com>
2020-11-17 18:29:10 +0300

    add date of start to the logs

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var { config, app } = require('./server');
 var port = process.env.PORT || config.port || 9999;
 
 app.listen(port, null, function (err) {
+  console.log(new Date)
   console.log('Gatekeeper, at your service: http://localhost:' + port);
 });
